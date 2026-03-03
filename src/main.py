e7d044f0c22adb8c859fb6d0ba1a9b0f8e34c60a
analystbruh <chris.martin.data@gmail.com>
2026-03-03 10:16:48 -0500

    feat: dont process message older than 10 secs

--- src/main.py
+++ src/main.py
@@ -259,8 +259,8 @@ async def webhook_post(data: Request):
             if timestamp:
                 timediff = datetime.now() - datetime.fromtimestamp(int(timestamp))
                 print('MESSAGE AGE:', timediff.seconds, "SECONDS")
-                if timediff.seconds > 2 * 60:
-                    # 2 minutes
+                if timediff.seconds > 10:
+                    # 10 seconds
                     return "old message"
             body = messages[0].get("text", {}).get("body")
             waid = messages[0].get("id")
