c2f305963825b60e47f52c8197bd81654518e278
analystbruh <chris.martin.data@gmail.com>
2026-03-07 14:10:46 -0500

    feat: make sure last day is included

--- src/funcs/gcloud_task.py
+++ src/funcs/gcloud_task.py
@@ -90,7 +90,7 @@ async def create_http_tasks(start_date, number):
     start_datetime = start_date_obj + datetime.timedelta(hours=13)
     json_payload = { "number": number }
     job_marker = uuid.uuid4()
-    for i in range(5):
+    for i in range(6):
         jp = {
             "exercises": workouts[f"day_{i}"],
             **json_payload
